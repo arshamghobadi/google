598eeead0008b115d9375816a22ce93bd19cbc08
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-03-30 20:05:54 +0100

    create google next and compelte navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import React from 'react';
+import { CgMenuGridR } from 'react-icons/cg';
+function Header() {
+  return (
+    <header>
+      <nav className="flex flex-row-reverse p-5 items-center space-x-5 ">
+        <button className=" hover:shadow-md hover:brightness-105 rounded-md bg-blue-600 text-white px-6 py-2">
+          Sign in
+        </button>
+        <div className=" flex p-2 items-center mx-auto hover:bg-gray-300 hover:rounded-full transition">
+          <CgMenuGridR className="" size={30} />
+        </div>
+        <Link className=" hover:underline" href="/">
+          Images
+        </Link>
+        <Link className=" hover:underline" href="/">
+          Gmail
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
