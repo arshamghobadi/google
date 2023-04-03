e0b5ada71b4d2bac62b143f5a306cf0a797460c3
Arsham Ghobadi <ghobadi.arsham@gmail.com>
2023-04-03 12:30:42 +0100

    fetching data with search engin

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,7 +1,19 @@
+import axios from 'axios';
 import React from 'react';
 
-function SearchPage() {
-  return <div>SearchPage</div>;
+async function SearchPage({ searchParams }) {
+  const response = await axios.get(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const { data } = response;
+
+  return (
+    <div>
+      {data?.items.map((item, i) => (
+        <div key={i}>{item.title}</div>
+      ))}
+    </div>
+  );
 }
 
 export default SearchPage;
